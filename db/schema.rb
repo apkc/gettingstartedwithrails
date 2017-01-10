7be5301a68b3e407587c6e89bb8a4cb4a291f058
apkc <avishkarpratapkc@gmail.com>
2017-01-10 14:05:28 +0545

    rendered sidebar for projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109130818) do
+ActiveRecord::Schema.define(version: 20170110071838) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
+    t.integer  "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["task_id"], name: "index_comments_on_task_id"
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
